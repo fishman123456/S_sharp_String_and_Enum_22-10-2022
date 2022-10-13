437d0aae60850732385f539af074abf8c9e6f4fd
denis <106389581+fishman123456@users.noreply.github.com>
2022-10-13 17:17:49 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Пользователь вводит с клавиатуры некоторый текст.
+// Приложение должно изменять регистр первой буквы каждого
+// предложения на букву в верхнем регистре
+Console.Title = " Пример использования инструментов класса Console"; // оформление
+Console.BackgroundColor = ConsoleColor.Black; //изменяет цвет фона
+Console.ForegroundColor = ConsoleColor.DarkGreen;//изменяет цвет текста
+Console.SetWindowSize(100, 30);//устанавливаем размер окна консоли
+
+Console.WriteLine("Введите строку со знаками препинания. Использование точки обязательно:\t");
+Console.WriteLine(); // перенос на новую строку
+string str1 = Console.ReadLine();       
